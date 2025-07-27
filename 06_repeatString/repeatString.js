ccb44d406fc483bfe4d8609716d5e951eeea866c
Luca <171629213+LucaX877@users.noreply.github.com>
2025-07-27 18:03:52 +0100

    Complete exercise 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let newString = "";
+    if (0 > num) {
+        return "ERROR";
+    }
+    for (let i = 0; i < num; i++) {
+        newString += string;
+    } return newString;
 };
 
 // Do not edit below this line
