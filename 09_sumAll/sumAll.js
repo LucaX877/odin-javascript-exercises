891973c84724372c01917de5c0f9898dfb574b4d
Luca <171629213+LucaX877@users.noreply.github.com>
2025-07-27 18:55:00 +0100

    Complete exercise 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    let sum = 0;
+    if ( a < 0 || b < 0 || !(typeof a == "number") || !(typeof b == "number") || !(Math.floor(a) == a) || !(Math.floor(b) == b)) return "ERROR";
+    for (let i = Math.min(a,b); i <= Math.max(a,b); i++) {
+        sum += i;
+    } return sum;
 };
 
 // Do not edit below this line
