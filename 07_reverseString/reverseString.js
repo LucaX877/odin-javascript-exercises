383deb391328de7578fff3e8c53f9476354ff827
Luca <171629213+LucaX877@users.noreply.github.com>
2025-07-27 18:09:14 +0100

    Complete exercise 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let array = string.split("").reverse();
+    let newString = array.join("");
+    return newString;
 };
 
 // Do not edit below this line
