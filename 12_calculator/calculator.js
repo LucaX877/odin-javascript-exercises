28c8657c17f9648b6d249b31896c914801dc9266
Luca <171629213+LucaX877@users.noreply.github.com>
2025-08-01 16:35:09 +0100

    Complete exercise 12_calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((sum,current) => sum += current,0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((answer,current) => answer *= current,1);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let answer = 1;
+  for (let i = 1; i <= num; i++) {
+    answer *= i;
+  } return answer;
 };
 
 // Do not edit below this line
