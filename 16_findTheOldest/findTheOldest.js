4de452c027ce93f45746312c6a928e68d7091754
Luca <171629213+LucaX877@users.noreply.github.com>
2025-08-01 18:50:23 +0100

    Complete exercise 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    array.sort((a,b) => {
+        const date = new Date();
+        if (a.yearOfDeath && b.yearOfDeath) {
+            return (a.yearOfDeath - a.yearOfBirth) - (b.yearOfDeath - b.yearOfBirth)
+        } else if (!a.yearOfDeath) return (date.getFullYear() - a.yearOfBirth) - (b.yearOfDeath - b.yearOfBirth);
+        else if (!b.yearOfDeath) return (a.yearOfDeath - a.yearOfBirth) - (date.getFullYear() - b.yearOfBirth);
+    })
+    return array.at(-1);
 };
 
 // Do not edit below this line
