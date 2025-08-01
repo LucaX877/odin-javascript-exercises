4be150ffb6908e694a3c324bea0e0072224d86e8
Luca <171629213+LucaX877@users.noreply.github.com>
2025-08-01 18:33:44 +0100

    Complete exercise 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,11 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    arr = [1,1]
+    for (let i = 1; i < num; i++) {
+        arr.push(arr[i]+arr[i-1]);
+    }
+    if (num == 0) return 0;
+    else if (num < 0) return "OOPS";
+    return arr[num-1]
 
 };
 
