d51f46497a30646538f734f8957f07d0e45bf1ae
Luca <171629213+LucaX877@users.noreply.github.com>
2025-08-01 17:14:26 +0100

    Complete exercise 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+  let rex = new RegExp('[a - zA - Z0 - 9]');
+  let newWord = word.toLowerCase();
+  newWord = newWord.split('');
+  for (let i = 0; i < newWord.length; i++) {
+    if (newWord[i] == !rex) {
+      newWord.splice(i, 1);
+    }
+    if (newWord[i] == '.') {
+      newWord.splice(i, 1);
+    }
+    if (newWord[i] == ',') {
+      newWord.splice(i, 1);
+    }
+    if (newWord[i] == "!") {
+        newWord.splice(i,1);
+    }
+    if (newWord[i] == ' ') {
+      newWord.splice(i, 1);
+    }
+  }
+  return newWord.join('') == newWord.reverse().join('');
 };
 
 // Do not edit below this line
